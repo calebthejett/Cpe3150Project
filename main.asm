82185b6b81f312b7f38262f6c2d83b1e7375becf
Caleb Jett <ccj7c5@mst.edu>
2018-11-28 23:13:00 -0600

    Add WIP on 3-bit booths' alg

--- main.asm
+++ main.asm
@@ -2,9 +2,12 @@
 MCAND_HI	EQU	0x00	; high byte of multiplicand (top nibble=0000)
 MCAND_LO	EQU	0x42	; low byte of multiplicand 
 
-BCAND_HI	EQU	R5	; the mcand must be shifted left four bits for
-BCAND_LO	EQU	R4	; booths alg. The code does this automatically
+BCAND_HI	EQU	0x50	; the mcand must be shifted left four bits for
+BCAND_LO	EQU	0x51	; booths alg. The code does this automatically
 				; and stores the result here
+
+BCAND2X_HI	EQU	0x52	; the mcand must be shifted left four bits for
+BCAND2X_LO	EQU	0x53	; booths alg. The code does this automatically
 
 MLIER_HI	EQU	0x00	; high byte of multiplier (top nibble=0000)
 MLIER_LO	EQU	0x02	; low byte of multiplier
@@ -15,6 +18,110 @@ PR_HI		EQU	R2	; high byte of product storage location
 PR_MID		EQU	R1	; mid byte of product sotrage location
 PR_LO		EQU	B	; low byte of product storage location
 
+; 3-BIT BOOTH'S ALGORITHM
+BOOTHS3:
+	MOV	R0,	#12		; this works for up to 12 bits
+	MOV	R7,	#0x04		; We need to shift mcand left four bits
+	MOV	BCAND_LO,	#MCAND_LO	; load mcand into shifted spots
+	MOV	BCAND_HI,	#MCAND_HI
+	MOV	PR_HI, 	#0		; clear high byte of output
+	MOV	PR_MID,	#MLIER_HI	; move multiplier into low 12b
+	MOV	PR_LO, 	#MLIER_LO
+SHIFT:
+	MOV	C,	F0
+	MOV	A,	BCAND_LO	; load low-byte
+	RLC	A			; shift left
+	MOV	BCAND_LO,	A	; put low byte back
+	MOV	A,	BCAND_HI	; load high byte
+	RLC	A			; rotate left
+	MOV	BCAND_HI,	A	; put high byte back
+	DJNZ	R7,	SHIFT		; keep shifting until zero
+					; over four bits; this is its counter
+	MOV	A,	BCAND_LO
+	CLR	C
+	RLC	A			; multiply low mcand by 2
+	MOV	BCAND2X_LO,	A	; store it for the 2x multiplication
+	MOV	A,	BCAND_HI
+	RLC	A
+	MOV	BCAND2X_HI,	A
+B_LOOP:
+	MOV 	A,	PR_LO		; pull low byte into accumulator
+	RLC	A			; rotate carry into bit 0
+	ANL	A,	#0x07		; mask just the last three bits
+	JZ	B3_SHFT			; just shift if 000
+	CJNE	A,	#0x01,	NOT1 
+	SJMP	ADD1X
+NOT1:	CJNE	A,	#0x02,	NOT2 
+	SJMP	ADD1X
+NOT2:	CJNE	A,	#0x03,	NOT3 
+	SJMP	ADD2X
+NOT3:	CJNE	A,	#0x04,	NOT4
+	SJMP	SUB2X
+NOT4:	CJNE	A,	#0x05,	NOT5 
+	SJMP	SUB1X
+NOT5:	CJNE	A,	#0X06,	B3_SHFT 
+	SJMP	SUB1X		
+	
+ADD2X:
+	MOV	A,	PR_MID		; move mid byte to accumulator
+	ADD	A,	BCAND2X_LO	; add low byte of multiplicand
+	MOV	PR_MID,	A		; put the sum back in to product
+	MOV	A,	PR_HI		; move high byte to accumulator
+	ADDC	A,	BCAND2X_HI	; add high byte w/ carry
+	MOV	PR_HI, A
+	SJMP	B3_SHFT
+ADD1X:	
+	MOV	A,	PR_MID		; move mid byte to accumulator
+	ADD	A,	BCAND_LO	; add low byte of multiplicand
+	MOV	PR_MID,	A		; put the sum back in to product
+	MOV	A,	PR_HI		; move high byte to accumulator
+	ADDC	A,	BCAND_HI	; add high byte w/ carry	
+	MOV	PR_HI, A
+	SJMP	B3_SHFT
+SUB2X:
+	SUBB	A,	BCAND2X_LO	; sub low byte of multiplicand
+	MOV	PR_MID,	A		; put the sum back in to product
+	MOV	A,	PR_HI		; move high byte to accumulator
+	SUBB	A,	BCAND2X_HI	; add high byte w/ carry
+	MOV	PR_HI,	A
+	SJMP	B3_SHFT		
+SUB1X:
+	SUBB	A,	BCAND_LO	; sub low byte of multiplicand
+	MOV	PR_MID,	A		; put the sum back in to product
+	MOV	A,	PR_HI		; move high byte to accumulator
+	SUBB	A,	BCAND_HI	; add high byte w/ carry
+	MOV	PR_HI,	A
+	SJMP	B3_SHFT	
+B3_SHFT:
+	MOV	A,	PR_HI
+	RLC	A
+	MOV	F0,	C		; figure out the leftmost bit to copy it
+	RRC	A
+	MOV	C,	F0
+	RRC	A			; shift high byte right
+	MOV	PR_HI,	A		; put it in product register
+	MOV	A,	PR_MID		; load in middle byte
+	RRC	A			; shift mid byte right
+	MOV	PR_MID,	A		; put it in product register
+	MOV	A,	PR_LO		; load in low byte
+	RRC	A			; shift low byte right
+	MOV	PR_LO,	A		; put it in product register
+
+	MOV	A,	PR_HI		; shift again
+	RLC	A
+	MOV	F0,	C		; figure out the leftmost bit to copy it
+	RRC	A
+	MOV	C,	F0
+	RRC	A			; shift high byte right
+	MOV	PR_HI,	A		; put it in product register
+	MOV	A,	PR_MID		; load in middle byte
+	RRC	A			; shift mid byte right
+	MOV	PR_MID,	A		; put it in product register
+	MOV	A,	PR_LO		; load in low byte
+	RRC	A			; shift low byte right
+	MOV	PR_LO,	A		; put it in product register
+	DJNZ	R0,	B_LOOP		; keep looping for every bit in mlier
+	END
 
 ; 2-BIT BOOTH'S ALGORITHM
 BOOTHS:
